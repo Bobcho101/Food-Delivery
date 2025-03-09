efa54051642d262f60f4cdfa1212f832cd34765b
Bobcho101 <bgbobi878@gmail.com>
2025-03-09 12:18:24 +0200

    made fetch one food fucntiion

--- client/src/services/foodService.js
+++ client/src/services/foodService.js
@@ -7,3 +7,13 @@ export async function fetchAllFood() {
         console.log(err.message);
     }
 }
+
+export async function fetchOneFood(id) {
+    try{
+        const res = await fetch(`http://localhost:3001/food/${id}`);
+        const data = await res.json();
+        return data;
+    } catch(err){
+        console.log(err.message);
+    }
+}
