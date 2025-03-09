533fb8e7b43d1fddd69327f395a4ad042d9dc5c6
Bobcho101 <bgbobi878@gmail.com>
2025-03-09 11:16:10 +0200

    added dev script

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ import cors from 'cors';
 const app = express();
 const PORT = 3001;
 
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+
 app.listen(PORT, console.log(`http://localhost:${PORT}`));
 
 
